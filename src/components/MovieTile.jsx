3dc2984f3791193ccbfd315d999144b93c5a039a
robby oktahidayat <robbyoktahidayat@gmail.com>
2022-07-21 21:42:28 +0700

    remove: dummy Movie

--- src/components/MovieTile.jsx
+++ src/components/MovieTile.jsx
@@ -1,23 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const dummyMovie = {
-  adult: false,
-  backdrop_path: "/qTkJ6kbTeSjqfHCFCmWnfWZJOtm.jpg",
-  genre_ids: [10751, 16, 12, 35, 14],
-  id: 438148,
-  original_language: "en",
-  original_title: "Minions: The Rise of Gru",
-  overview:
-    "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
-  popularity: 11510.186,
-  poster_path: "/wKiOkZTN9lUUUNZLmtnwubZYONg.jpg",
-  release_date: "2022-06-29",
-  title: "Minions: The Rise of Gru",
-  video: false,
-  vote_average: 7.6,
-  vote_count: 391,
-};
 const MovieTile = ({ dataMovie }) => {
   // console.log(dataMovie);
 
--- src/components/protectedComponent.jsx
+++ src/components/protectedComponent.jsx
@@ -1,15 +1,7 @@
-// Sekarang pada ProtectedComponent, kita tetap harus menggunakan useEffect
-// Agar terlepas dari Warning yang diberikan
-// (useNavigate harus di dalam useEffect)
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
-// Di sini kita akan akan menggunakan hooks:
-// - untuk mendeteksi user sudah login (useAuthState)
-// - untuk memaksa navigasi bila user belum login dengan (useNavigate)
 import { useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
-
-// Untuk bisa menggunakan useAuthState, kita membutuhkan auth dari authentication/firebase
 import { auth } from "../authentication/firebase";
 
 const ProtectedComponent = ({ children }) => {
@@ -18,7 +10,6 @@ const ProtectedComponent = ({ children }) => {
   const [user, isLoading] = useAuthState(auth);
 
   useEffect(() => {
-    // console.log(user);
     if (isLoading) {
       return;
     } else if (!user) {
